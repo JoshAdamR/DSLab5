10fe5aa9b1a8cc6abfd3a29aece1139cc8768f58
JoshAdamR <94948671+JoshAdamR@users.noreply.github.com>
2023-03-07 08:57:18 +0800

    Add files via upload

--- app.R
+++ app.R
@@ -47,3 +47,51 @@ ui <- fluidPage(
     )
   )
 )
+
+# Define the server
+server <- function(input, output) {
+  
+  # Create a reactive timer with interval 1000 milliseconds
+  autoInvalidate <- reactiveTimer(1000)
+  
+  # Display the current time
+  output$time <- renderText({
+    paste("The current time is:", Sys.time())
+  })
+  
+  datasetInput <- reactive({
+    switch (
+      input$dataset,
+      "rock" = rock,
+      "pressure" = pressure,
+      "cars" = cars,
+      "iris" = iris
+    )
+  })
+  
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  
+  output$view <- renderTable({
+    head(datasetInput(), n = input$obs)
+  })
+  
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(datasetInput(), file, row.names = FALSE)
+    }
+  )
+  
+  # Trigger the reactive timer every second to update the current time
+  observe({
+    autoInvalidate()
+  })
+}
+
+# Run the app
+shinyApp(ui = ui, server = server)
